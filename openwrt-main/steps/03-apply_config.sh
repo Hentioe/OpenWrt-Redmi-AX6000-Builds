b3c86164b7af24b7ba2fe6225e6015ac6f3c76e4
Hentioe <meow.i5.br@gmail.com>
2023-10-25 12:12:16 +0800

    Add configs

--- openwrt-main/steps/03-apply_config.sh
+++ openwrt-main/steps/03-apply_config.sh
@@ -7,7 +7,17 @@ OPENWRT_BRANCH=main
 
 cd "$ROOTDIR/build/openwrt"
 
-# 将种子配置文件复制为当前的 .config
+# 创建配置文件目录
+rm -rf files/etc/config/
+mkdir -p files/etc/config/
+
+# 复制配置文件
+cp $ROOTDIR/openwrt-$OPENWRT_BRANCH/config/system files/etc/config/
+cp $ROOTDIR/openwrt-$OPENWRT_BRANCH/config/firewall files/etc/config/
+cp $ROOTDIR/openwrt-$OPENWRT_BRANCH/config/wireless files/etc/config/
+
+# 复制 menuconfig 的配置
 cp $ROOTDIR/openwrt-$OPENWRT_BRANCH/seed/$1.seed .config
 
+# 应用 menuconfig 修改
 make defconfig
